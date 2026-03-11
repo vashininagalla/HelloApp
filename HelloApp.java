18e6658497f11f5655b4f70427b18d42429150ac
vashininagalla <vashininagalla@email.com>
2026-03-11 13:27:06 +0530

    UC3: Display Hello with command-line argument or default World

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,13 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name = args[0];
+        String name;
+
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
 
         System.out.println("Hello " + name);
     }
