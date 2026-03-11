4985c2cfc4f7508b037e56232bb0e00155a508e4
vashininagalla <vashininagalla@email.com>
2026-03-11 13:23:59 +0530

    UC2: Display Hello with command-line argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,8 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String name = args[0];
+
+        System.out.println("Hello " + name);
     }
 }
