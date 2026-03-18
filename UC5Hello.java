3112415d6424a6d950b6ea3477a56d777b8cc70f
vashininagalla <vashininagalla@email.com>
2026-03-18 12:50:42 +0530

    UC5 using enhanced for loop

---
+++ UC5Hello.java
@@ -0,0 +1,12 @@
+public class UC5Hello {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
+        }
+    }
+}
