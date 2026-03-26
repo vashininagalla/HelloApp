4d6e637f880cb9762710fe052219551c49b15a36
vashininagalla <vashininagalla@email.com>
2026-03-26 14:35:32 +0530

    UC7 Maven project

---
+++ src/main/java/org/example/HelloMultiArgs.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class HelloMultiArgs {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(" ", args);
+            System.out.println("Hello, " + names);
+        }
+
+    }
+}
